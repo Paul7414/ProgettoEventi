d87acc0992044700288d26ce5520e3c3568367e0
Paul7414 <paolovinci7414@gmail.com>
2025-05-26 17:42:03 +0200

    cambiata navbar e tolto tasto scopri

--- frontend/src/lib/CardEvento.svelte
+++ frontend/src/lib/CardEvento.svelte
@@ -67,10 +67,7 @@
       </div>
     {/if}
     
-    <button class="cta-button">
-      Scopri di più
-      <svg viewBox="0 0 24 24" class="arrow-icon"><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/></svg>
-    </button>
+  
   </div>
 </div>
 
--- frontend/src/lib/Navbar.svelte
+++ frontend/src/lib/Navbar.svelte
@@ -38,28 +38,7 @@
       <span on:click={handleLogoClick}>Eventers</span>
     </div>
     
-    <div class="search-container">
-      <div class="search-box">
-        <svg viewBox="0 0 24 24" class="search-icon">
-          <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-        </svg>
-        <input 
-          type="text" 
-          placeholder="Cerca per luogo..." 
-          bind:value={luogoSelezionato}
-          list="luoghi"
-          class="search-input"
-        />
-        <datalist id="luoghi">
-          {#each luoghiDisponibili as luogo}
-            <option value={luogo} />
-          {/each}
-        </datalist>
-        <button on:click={cercaEventi} class="search-button">
-          Cerca
-        </button>
-      </div>
-    </div>
+    
     
     <div class="profile-section">
       {#if $authStore.isAuthenticated}
